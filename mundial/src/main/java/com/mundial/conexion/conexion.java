16545cb29fd73b800b2c8c41a4efbe2cd0aaadad
Dave <herbertburrion650@gmail.com>
2026-05-06 15:01:52 -0600

    ignorar archivo conexion

---
+++ mundial/src/main/java/com/mundial/conexion/conexion.java
@@ -0,0 +1,15 @@
+package com.mundial.conexion; 
+//CONEXION ENTRE JAVA Y POSTGRESQL
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class conexion {
+    private static final String URL = "jdbc:postgresql://localhost:5432/Proyecto_Mundial";
+    private static final String USER ="postgres";
+    private static final String PASSWORD = "Abduzcan123";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
