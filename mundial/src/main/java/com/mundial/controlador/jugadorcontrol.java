ffa60d92de8aff1c8a74460250d2848a86915273
Dave <herbertburrion650@gmail.com>
2026-05-07 14:13:39 -0600

    cambio de letra de nuevo

--- mundial/src/main/java/com/mundial/controlador/jugadorcontrol.java
+++ mundial/src/main/java/com/mundial/controlador/jugadorcontrol.java
@@ -4,19 +4,21 @@ import com.mundial.servicio.JugadorService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-    
-@Controller
-public class jugadorcontrol {
 
+
+
+@Controller
+public class jugadorControl {
     private final JugadorService jugadorService;
 
-    public jugadorcontrol(JugadorService jugadorService) {
+    public jugadorControl(JugadorService jugadorService) {
         this.jugadorService = jugadorService;
     }
 
     @GetMapping("/jugador")
-    public String listarJugadores(Model model) {
+    public String listarJugadores(Model model){
         model.addAttribute("jugadores", jugadorService.listarJugadores());
-        return "jugador"; 
-    }
+        return "jugador"; //nombre de la plantilla HTML
+    
+}
 }
