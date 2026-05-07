93d84c73c21da1d67a408ade68d8cc1ae81a7138
Dave <herbertburrion650@gmail.com>
2026-05-07 14:17:47 -0600

    Ahora si corregido error mayuscula

--- mundial/src/main/java/com/mundial/controlador/jugadorcontrol.java
+++ mundial/src/main/java/com/mundial/controlador/jugadorcontrol.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
-public class jugadorControl {
+public class jugadorcontrol {
     private final JugadorService jugadorService;
 
-    public jugadorControl(JugadorService jugadorService) {
+    public jugadorcontrol(JugadorService jugadorService) {
         this.jugadorService = jugadorService;
     }
 
